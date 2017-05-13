e0934bd8319dd8b1dc7da036ebacb2b35526cdd3
liuppboy <liuppboy@gmail.com>
2017-05-14 01:53:53 +0800

    adjust aigan

--- AIGAN/aigan.py
+++ AIGAN/aigan.py
@@ -17,7 +17,7 @@ from activations import lrelu
 from utils import load_model, save_grid
 
 class AIGAN(object):
-    def __init__(self, sess, batch_size=32, iter_steps=100000, z_dim=100, learning_rate=1e-3, 
+    def __init__(self, sess, batch_size=64, iter_steps=100000, z_dim=100, learning_rate=1e-3, 
                  sample_size=100, beta1=0.5, is_load_model=False, dataset_name='mnist', 
                  model_name='aigan_model', checkpoint_dir='checkpoints', summary_dir='summary', 
                  sample_dir='sample', read_mode='from_file'):
@@ -67,7 +67,7 @@ class AIGAN(object):
                 
         
         np.random.seed(100)
-        self.sample_z = tf.constant(np.random.normal(size=[100, self.z_dim]).astype(np.float32)) 
+        self.sample_z = tf.constant(np.random.normal(size=[400, self.z_dim]).astype(np.float32)) 
         self.z_input = tf.random_normal(shape=[self.batch_size, self.z_dim], name='z_input')
         
     
@@ -97,16 +97,25 @@ class AIGAN(object):
                 net['conv2'] = slim.conv2d(net['conv1'], 128, scope='conv2')
                 net['conv3'] = slim.conv2d(net['conv2'], 256, scope='conv3')
                 
-                # regression z
-                net['conv4'] = slim.conv2d(net['conv3'], 128, scope='conv4')
-                net['conv4'] = slim.flatten(net['conv4'])
-                net['z_regression'] = slim.fully_connected(net['conv4'], self.z_dim, activation_fn=None, trainable=train, scope='z_regression')
-                
                 # true or false
                 net['conv3'] = slim.flatten(net['conv3'])
                 net['fc'] = slim.fully_connected(net['conv3'], 1, activation_fn=None, trainable=train, scope='fc')
                     
         return net
+    
+    def encoder(self, x, train, reuse=None):
+        with tf.variable_scope('encoder', reuse=reuse) as scope: 
+            with slim.arg_scope([slim.conv2d], activation_fn=lrelu, kernel_size=3, stride=2, padding='SAME', 
+                                normalizer_fn=batch_norm, normalizer_params={'train': train}, trainable=train):
+                net = {}
+                net['conv1'] = slim.conv2d(x, 64, scope='conv1')
+                net['conv2'] = slim.conv2d(net['conv1'], 128, scope='conv2')
+                net['conv3'] = slim.conv2d(net['conv2'], 256, scope='conv3')
+                
+                # regression z
+                net['conv3'] = slim.flatten(net['conv3'])
+                net['z_regression'] = slim.fully_connected(net['conv3'], self.z_dim, activation_fn=None, trainable=train, scope='z_regression')       
+        return net        
     
     def bulid_model(self):        
         self.global_step = tf.Variable(0, trainable=False)
@@ -116,36 +125,43 @@ class AIGAN(object):
         
         self.d_net_fake = self.discriminator(self.G, train=True)
         self.d_net_ture = self.discriminator(self.batch_data, train=True, reuse=True)
-        self.img_reconstruct = self.generator(self.d_net_ture['z_regression'], train=True, reuse=True)['deconv3']
+        self.e_net_true = self.encoder(self.batch_data, train=True)
+        self.e_net_fake = self.encoder(self.G, train=True, reuse=True)
+        self.img_reconstruct = self.generator(self.e_net_true['z_regression'], train=True, reuse=True)['deconv3']
         
         self.D_true = self.d_net_ture['fc']
         self.D_fake = self.d_net_fake['fc']
         
         self.d_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='discriminator')
         self.g_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='generator')
+        self.e_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='encoder')
         
         all_vars = tf.global_variables()
         self.moving_mean_vars = [var for var in all_vars if 'moving_mean' in var.name]
         self.moving_variance_vars = [var for var in all_vars if 'moving_variance' in var.name]
-        self.save_vars = self.d_vars + self.g_vars + self.moving_mean_vars + self.moving_variance_vars + [self.global_step]
+        self.save_vars = self.d_vars + self.g_vars + self.e_vars + self.moving_mean_vars + self.moving_variance_vars + [self.global_step]
         self.saver = tf.train.Saver(var_list=self.save_vars, max_to_keep=200)
         
         self.g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.D_fake, labels=tf.ones_like(self.D_fake)))
         self.d_loss_true =  tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.D_true, labels=tf.ones_like(self.D_true)))
         self.d_loss_fake =  tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.D_fake, labels=tf.zeros_like(self.D_fake))) 
         self.d_loss = self.d_loss_true + self.d_loss_fake
-        self.z_regression_loss = tf.reduce_mean(tf.abs(self.z_input - self.d_net_fake['z_regression']), name='z_regression_loss')
+        self.z_regression_loss = tf.reduce_mean(tf.abs(self.z_input - self.e_net_fake['z_regression']), name='z_regression_loss')
         self.autoencoder_loss = tf.reduce_mean(tf.square(self.batch_data - self.img_reconstruct), name='autoencoder_loss')
         
         tf.summary.scalar('d_loss_true', self.d_loss_true)
         tf.summary.scalar('d_loss_fake', self.d_loss_fake)
         tf.summary.scalar('g_loss', self.g_loss)
-        tf.summary.scalar('d_loss', self.d_loss)        
+        tf.summary.scalar('d_loss', self.d_loss)
+        tf.summary.scalar('z_regression_loss', self.z_regression_loss)
+        tf.summary.scalar('autoencoder_loss', self.autoencoder_loss)
         
-        
-        self.d_optim = tf.train.AdamOptimizer(self.learning_rate, self.beta1).minimize(self.d_loss + self.z_regression_loss, 
+        lr = tf.train.polynomial_decay(self.learning_rate, self.global_step, decay_steps=1e5, end_learning_rate=1e-5)
+        self.d_optim = tf.train.AdamOptimizer(lr, self.beta1).minimize(self.d_loss, 
                                             global_step=self.global_step, var_list=self.d_vars)
-        self.g_optim = tf.train.AdamOptimizer(self.learning_rate, self.beta1).minimize(self.g_loss + self.autoencoder_loss, var_list=self.g_vars)
+        self.g_optim = tf.train.AdamOptimizer(lr, self.beta1).minimize(self.g_loss, var_list=self.g_vars)
+        self.e_optim = tf.train.AdamOptimizer(lr, self.beta1).minimize(10 * self.z_regression_loss + self.autoencoder_loss, 
+                                                                       var_list=self.g_vars + self.e_vars)
         
     def sampler(self, sample_z):
         sample_fake_images = self.generator(sample_z, train=False, reuse=True)['deconv3']
@@ -178,11 +194,11 @@ class AIGAN(object):
                 batch_data = batch_data * 2 - 1.
                 batch_data = np.reshape(batch_data, [self.batch_size, 28, 28, 1])
                 batch_z = np.random.normal(size=[self.batch_size, self.z_dim])
-                _, _, d_loss, g_loss, d_loss_true, d_loss_fake = self.sess.run(
-                    [self.d_optim, self.g_optim, self.d_loss, self.g_loss, self.d_loss_true, self.d_loss_fake], 
+                _, _, _, d_loss, g_loss, d_loss_true, d_loss_fake = self.sess.run(
+                    [self.d_optim, self.g_optim, self.e_optim, self.d_loss, self.g_loss, self.d_loss_true, self.d_loss_fake], 
                 feed_dict={self.batch_data: batch_data})
         
-                if np.mod(step, 10) == 0:
+                if np.mod(step, 20) == 0:
                     print('step: %d time: %.6fs d_loss: %.6f, g_loss: %.6f, d_loss_true: %.6f, d_loss_fake: %.6f' % 
                           (step, time.time() - start_time, d_loss, g_loss, d_loss_true, d_loss_fake))
                 
@@ -192,7 +208,7 @@ class AIGAN(object):
                     
                     sample_fake_images = self.sess.run(self.sample_fake_images)
                     save_path = '%s/step_%d.png' %(self.sample_dir, step)
-                    save_grid(sample_fake_images, [10, 10], save_path)
+                    save_grid(sample_fake_images, [20, 20], save_path)
                 
                 if np.mod(step, 1000) == 0:
                     self.saver.save(self.sess, '/'.join([self.checkpoint_dir, self.model_name]), global_step=step)
@@ -201,19 +217,23 @@ class AIGAN(object):
             threads = tf.train.start_queue_runners(sess=self.sess, coord=coord) 
             for step in range(start_step, self.iter_steps):
                 batch_z = np.random.normal(size=[self.batch_size, self.z_dim])
-                _, _, d_loss, g_loss, d_loss_true, d_loss_fake = self.sess.run(
-                    [self.d_optim, self.g_optim, self.d_loss, self.g_loss, self.d_loss_true, self.d_loss_fake])
-                if np.mod(step, 10) == 0:
-                    print('step: %d time: %.6fs d_loss: %.6f, g_loss: %.6f, d_loss_true: %.6f, d_loss_fake: %.6f' % 
-                          (step, time.time() - start_time, d_loss, g_loss, d_loss_true, d_loss_fake))
+                _, _, _, d_loss, g_loss, d_loss_true, d_loss_fake, z_regression_loss, autoencoder_loss = \
+                    self.sess.run([self.d_optim, self.g_optim, self.e_optim, self.d_loss, self.g_loss, self.d_loss_true, 
+                     self.d_loss_fake, self.z_regression_loss, self.autoencoder_loss])
+                self.sess.run([self.g_optim, self.e_optim])
+                self.sess.run([self.g_optim, self.e_optim])
+                if np.mod(step, 20) == 0:
+                    print('step: %d time: %.6fs d_loss: %.6f, g_loss: %.6f, d_loss_true: %.6f, d_loss_fake: %.6f, z_regression_loss: %.6f, autoencoder_loss: %.6f' % 
+                          (step, time.time() - start_time, d_loss, g_loss, d_loss_true, 
+                           d_loss_fake, z_regression_loss, autoencoder_loss))
                 
                 if np.mod(step, 100)==0:
-                    summary_str = self.sess.run(merge_summary)
-                    summary_writer.add_summary(summary_str, global_step=step)
+                    #summary_str = self.sess.run(merge_summary)
+                    #summary_writer.add_summary(summary_str, global_step=step)
                     
                     sample_fake_images = self.sess.run(self.sample_fake_images)
                     save_path = '%s/step_%d.png' %(self.sample_dir, step)
-                    save_grid(sample_fake_images, [10, 10], save_path)
+                    save_grid(sample_fake_images, [20, 20], save_path)
                 
-                if np.mod(step, 1000) == 0:
-                    self.saver.save(self.sess, '/'.join([self.checkpoint_dir, self.model_name]), global_step=step)            
+                #if np.mod(step, 1000) == 0:
+                    #self.saver.save(self.sess, '/'.join([self.checkpoint_dir, self.model_name]), global_step=step)            
